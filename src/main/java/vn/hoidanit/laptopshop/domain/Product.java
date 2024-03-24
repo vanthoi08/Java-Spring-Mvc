720b3553e6e57e8b04d9c2bac630b6a86c3d83b6
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-03-24 22:30:56 +0900

    video 105

--- src/main/java/vn/hoidanit/laptopshop/domain/Product.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Product.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.laptopshop.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,6 +9,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -30,7 +32,8 @@ public class Product {
     private String image;
 
     @NotNull
-    @Size(min = 1, message = "detailDesc không được để trống")
+    @NotEmpty(message = "detailDesc không được để trống")
+    @Column(columnDefinition = "MEDIUMTEXT")
     private String detailDesc;
 
     @NotNull
