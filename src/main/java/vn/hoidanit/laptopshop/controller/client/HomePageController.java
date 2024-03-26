d4f22b07bff70131ef07ee82591e994408f74bb7
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-03-26 21:58:01 +0900

    video 109

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -2,19 +2,35 @@ package vn.hoidanit.laptopshop.controller.client;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
 import vn.hoidanit.laptopshop.service.ProductService;
+import vn.hoidanit.laptopshop.service.UserService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomePageController {
-    private final ProductService productService;
 
-    public HomePageController(ProductService productService) {
+    private final ProductService productService;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public HomePageController(
+            ProductService productService,
+            UserService userService,
+            PasswordEncoder passwordEncoder) {
         this.productService = productService;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/")
@@ -26,7 +42,31 @@ public class HomePageController {
 
     @GetMapping("/register")
     public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
         return "client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute("registerUser") RegisterDTO registerDTO) {
+
+        User user = this.userService.registerDTOtoUser(registerDTO);
+        // chuyển đổi mật khẩu
+        String hashPassword = this.passwordEncoder.encode(user.getPassword());
+
+        user.setPassword(hashPassword);
+
+        // set cứng role
+        user.setRole(this.userService.getRoleByName("USER"));
+
+        // Save
+        this.userService.handleSaveUser(user);
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        return "client/auth/login";
     }
 
 }
